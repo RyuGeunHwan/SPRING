4cf9d2f3772ca8677107b5f9f058f6f76e2f06c0
RyuGeunHwan <h46344@naver.com>
2022-06-01 00:24:09 +0900

    feat : 내용수정

--- 페이징 구현/Spring boot PageHandler.java
+++ 페이징 구현/Spring boot PageHandler.java
@@ -14,7 +14,7 @@ public class PageHandler {
 	private int endPage; // 현재 블록 마지막 페이지
 	private boolean hasPreviousPage;// 이전 버튼 유무
 	private boolean hasNextPage;// 다음 버튼 유무
-	private int nowBlock; // 현재 블록
+	private int nowBlock; // 현재 블록 == pageNum / navigatePages (feat. if절 이용해서 nowBlock++;)
 	private int lastBlock; // 마지막 블록
 	private int navigatePages; // 1블록당 페이지 갯수(ex) 1블록 : [1][2][3][4][5][6][7][8][9][10])
 	
@@ -29,6 +29,7 @@ public class PageHandler {
 		 *comment : 총 페이지 수
 		 */
 	public int calcPage(int total, int pageSize) {
+		// total : 총 게시글, pageSize : 한 페이지당 들어갈 게시글 수
 		int pages = total / pageSize;
 		if(total % pageSize > 0){
 			pages++;
@@ -47,6 +48,7 @@ public class PageHandler {
 		 *comment : 현재 페이지 블록 알아내기
 		 */
 	public void setNowBlock(int pageNum, int navigatePages) {
+		// pageNum : 현재 페이지, navigatePages : 한 블록에 보여질 페이지 수 ex) 1블록 => 1페이지~10페이지
 		this.nowBlock = pageNum / navigatePages;
 		if(pageNum % navigatePages > 0) {
 			this.nowBlock++;
@@ -61,8 +63,10 @@ public class PageHandler {
 		 * @date : 2022. 5. 31.
 		 *comment : 마지막 블록 알아내기
 		 */
-	public void setLastBlock(int total) {
+	public void setLastBlock(int total) { // total : 총 게시글 수
 		this.lastBlock = total / (this.navigatePages * this.pageSize);
+			// this.navigatePages : 필드변수에 있는 한 블록에 보여질 페이지 수, 
+			// this.pageSize : 필드변수에 있는 한 페이지에 보여질 게시글 수
 		if(total % (this.navigatePages * this.pageSize) > 0) {
 			this.lastBlock++;
 		}
@@ -77,7 +81,11 @@ public class PageHandler {
 		 * @date : 2022. 5. 31.
 		 *comment : 현재 블록의 첫 페이지
 		 */
-	public void setStartPage(int nowBlock) {
+	public void setStartPage(int nowBlock) { //nowBlock : 현재 해당되는 블록
+		// this.startPage : 현재 블록의 첫페이지 ex) 2블록 => 11페이지 ~ 20페이지
+		// ex) 한페이지당 게시글 5개씩 보여줄 수 있고 현재 페이지는 8페이지이다. 현재 블록은 2블록이다.
+		// this.startPage  = (2 * 5) - (5 - 1) == 6
+		// this.startPage = 6 => 해당 페이지 블록의 첫시작 페이지는 6페이지이다.
 		this.startPage = (nowBlock * this.navigatePages) - (this.navigatePages - 1);
 		
 	}
@@ -91,7 +99,8 @@ public class PageHandler {
 		 * @date : 2022. 5. 31.
 		 *comment : 현재 블록의 마지막 페이지
 		 */
-	public void setEndPage(int lastBlock, int nowBlock) {
+	public void setEndPage(int lastBlock, int total) {
+		// ?????????????????질문하기!!!
 		this.endPage = nowBlock * this.navigatePages;
 		if(nowBlock == this.lastBlock) {
 			this.endPage = total;
@@ -122,13 +131,102 @@ public class PageHandler {
 		}
 		
 	}
+
+
 	
 	
 	
 	
 	
 	
+	public int getTotal() {
+		return total;
+	}
+
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+
+	public int getPageNum() {
+		return pageNum;
+	}
+
+
+	public void setPageNum(int pageNum) {
+		this.pageNum = pageNum;
+	}
+
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+
+	public int getEndPage() {
+		return endPage;
+	}
+
+
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+
+
+	public boolean isHasPreviousPage() {
+		return hasPreviousPage;
+	}
+
+
+	public void setHasPreviousPage(boolean hasPreviousPage) {
+		this.hasPreviousPage = hasPreviousPage;
+	}
+
+
+	public boolean isHasNextPage() {
+		return hasNextPage;
+	}
+
+
+	public void setHasNextPage(boolean hasNextPage) {
+		this.hasNextPage = hasNextPage;
+	}
+
+
+	public int getNowBlock() {
+		return nowBlock;
+	}
+
+
+	public void setNowBlock(int nowBlock) {
+		this.nowBlock = nowBlock;
+	}
+
+
+	public int getNavigatePages() {
+		return navigatePages;
+	}
+
+
+	public void setNavigatePages(int navigatePages) {
+		this.navigatePages = navigatePages;
+	}
+
+
+	public int getStartPage() {
+		return startPage;
+	}
+
+
+	public int getLastBlock() {
+		return lastBlock;
+	}
 	
-	
-	
+
 }
